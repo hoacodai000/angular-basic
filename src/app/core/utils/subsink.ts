cc0ab7282761ca21830cdc8017123fe0cc08289c
hoacodai000 <trungvq94@gmail.com>
2021-06-01 11:16:48 +0700

    add utils subsink

---
+++ src/app/core/utils/subsink.ts
@@ -0,0 +1,41 @@
+
+const isFunction: (fn: any) => boolean = (fn: any) => typeof fn === 'function';
+
+export interface SubscriptionLike {
+  unsubscribe(): void;
+}
+
+export class SubSink {
+  private _subs: SubscriptionLike[] = [];
+  constructor() { }
+
+  public add(...subscriptions: SubscriptionLike[]): void {
+    this._subs = this._subs.concat(subscriptions);
+  }
+
+  set sink(subscription: SubscriptionLike) {
+    this._subs.push(subscription);
+  }
+
+  public unsubscribe(): void {
+    this._subs.forEach((sub: SubscriptionLike) => sub && isFunction(sub.unsubscribe) && sub.unsubscribe());
+    this._subs = [];
+  }
+}
+
+/**
+ * "takeUntil"
+ *
+ * import { Subject } from 'rxjs’;
+  import { takeUntil } from 'rxjs/operators'
+ * private readonly onDestroy$: Subject<void> = new Subject<void>()
+ *
+ * "takeUntil" after operators
+ * (api).pipe(takeUntil(this.onDestroy$)).subscribe()
+ *
+ * public ngOnDestroy(): void {
+ *   this.onDestroy$.next();
+ *   this.onDestroy$.complete();
+ * }
+ *
+ */
